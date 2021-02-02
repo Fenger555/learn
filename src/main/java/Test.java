2f332a8dafba75d8493401ed8318b0343d5619ae
Fenger <fengerzzz@163.com>
2021-02-02 18:06:20 +0800

    .

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,7 +2,9 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.ArrayList;
 import java.util.BitSet;
+import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
@@ -14,29 +16,15 @@ public class Test {
 
     public static void main(String[] args) {
 
-        String collect = Lists.newArrayList().stream().map(v -> v.toString()).collect(Collectors.joining(","));
-        System.out.println(collect);
+        List<String> arr1 = Lists.newArrayList();
+        List<String> arr2 = Lists.newArrayList();
 
-        int a=1,b=2,c=3;
-        a=b=c;
+        arr1.add("a");
+        arr2.add("a");
 
-        Random random = new Random();
+        arr1.removeAll(arr2);
 
-        retry:
-        for (;;) {
-            System.out.println("-----------------");
-            for (;;) {
-                int i = random.nextInt();
-                System.out.println(i);
-                if (i%13==0) {
-                    System.out.println("***" +i);
-                    continue retry;
-                }
-                break retry;
-            }
-        }
-
-        System.out.println("s");
+        System.out.println(arr1);
 
 
 
