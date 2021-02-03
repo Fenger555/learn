91f786bdb62d28f5ce05921ae92ee69425cf24d1
Fenger <fengerzzz@163.com>
2021-02-03 17:57:34 +0800

    test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,10 +2,11 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.ArrayList;
-import java.util.BitSet;
-import java.util.List;
-import java.util.Random;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -16,16 +17,16 @@ public class Test {
 
     public static void main(String[] args) {
 
-        List<String> arr1 = Lists.newArrayList();
-        List<String> arr2 = Lists.newArrayList();
+        System.out.println(DateTimeFormatter.ofPattern("E L  dd HH:mm:ss yyyy").format(LocalDateTime.now()));
+        System.out.println(DateTimeFormatter.ofPattern("E M  dd HH:mm:ss yyyy").parse("星期三 2  03 17:00:36 2021"));
 
-        arr1.add("a");
-        arr2.add("a");
-
-        arr1.removeAll(arr2);
-
-        System.out.println(arr1);
-
+        SimpleDateFormat sdf = new SimpleDateFormat("E L  dd HH:mm:ss yyyy");
+        try {
+            Date parse = sdf.parse("Fri Dec  4 12:48:46 2020");
+            System.out.println(parse.toString());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
 
     }
