7c1b341db83d115242d6ddf51c4433872c732831
Fenger <fengerzzz@163.com>
2021-02-07 17:24:15 +0800

    leetcode 460 LFU todo

---
+++ src/main/java/leetcode/Lc460_LFU.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+/**
+ * @author Fenger
+ * @date 2021-02-07 17:16
+ */
+public class Lc460_LFU {
+
+    public Lc460_LFU(int capacity) {
+
+    }
+
+    public int get(int key) {
+
+        return 0;
+    }
+
+    public void put(int key, int value) {
+
+    }
+
+}
